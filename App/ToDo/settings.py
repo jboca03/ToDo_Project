b5194f2bf6c34f2e50e54baa16d47ed96805862d
Jesus Bocanegra <bocajes37@gmail.com>
2024-11-06 09:41:40 -0600

    Added some ui changes and user restrictions

--- App/ToDo/settings.py
+++ App/ToDo/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "ToDo_App",
+    "ToDo_App.apps.TodoAppConfig",
 ]
 
 MIDDLEWARE = [
@@ -116,6 +116,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOGIN_URL = 'login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
