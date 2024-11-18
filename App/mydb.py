213f4a627fc9f99a5fe0fcc59cba15c269166283
Jesus Bocanegra <bocajes37@gmail.com>
2024-11-18 15:58:45 -0600

    changed password in db

--- App/mydb.py
+++ App/mydb.py
@@ -1,16 +1,16 @@
 import mysql.connector
 
 dataBase = mysql.connector.connect(
-	host = 'localhost',
-	user = 'root',
-	passwd = '@Morelos0103',
-	)
+    host='localhost',
+    user='root',
+    passwd='rootpassword',
+)
 
-#prepare cursor object (using the connector declare above)
-cursorObject = dataBase.cursor() 
+# prepare cursor object (using the connector declare above)
+cursorObject = dataBase.cursor()
 
-#create data base
+# create data base
 cursorObject.execute("CREATE DATABASE todoData")
 
-#Message in console to see if it worked 
+# Message in console to see if it worked
 print("Hello data base todoData")
